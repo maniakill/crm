a9f3884bf2e8ca351fba0492a8bec9fc80834076
maniakill <maniak_is_me@yahoo.com>
2014-04-07 22:30:53 +0300

    minor ajust

--- js/timetracker.js
+++ js/timetracker.js
@@ -124,6 +124,7 @@ app.directive('scroller',['project',function(project){
     restrict: 'C',
     link:  function(scope,element,attrs){
       element.bind('scroll',function(){
+        console.log(this.scrollTop , this.scrollTopMax)
         if(this.scrollTop == this.scrollTopMax){
           scope.loadMore();
         }
